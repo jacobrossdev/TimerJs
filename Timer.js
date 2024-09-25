f1aa348fbeb26dd18c93ddb07c0cebc78958214e
Jacob Ross <33608544+jacobrossdev@users.noreply.github.com>
2024-09-24 20:28:15 -0700

    Update Timer.js
    removed unused functions

--- Timer.js
+++ Timer.js
@@ -1,46 +1,37 @@
 // TIMER
 //---------------------------
 
+// Create a Timer Class
 var Timer = function(delayMs, callbackFunc) {
-  this.delayMs = delayMs;
-  this.callbackFunc = callbackFunc;
-  this.timerState = 'new';
-  this.paused = false;
+	this.delayMs = delayMs;
+	this.callbackFunc = callbackFunc;
+	this.timerState = 'new';
 }
 
+// Starts the timer
 Timer.prototype.start = function() {
-    if( this.tmr ) return;
+	if( this.tmr ) return;
 
-    var self = this;
-    this.timerState = 'running';
-    this.tmr = setInterval(function() { 
-    	console.log('timer running');
-    	self._handleTmr(); 
-    }, this.delayMs);
+	var self = this;
+	this.timerState = 'running';
+	this.tmr = setInterval(function() { 
+		console.log('timer running');
+		self._handleTmr(); 
+	}, this.delayMs);
 }
 
-Timer.prototype.pause = function(){
-
-	this.paused = true;
-	console.log('timer paused');
-}
-
-Timer.prototype.unpause = function(){
-
-	this.paused = false;
-	console.log('timer unpaused');
-}
-
+// Cancels the timer
 Timer.prototype.cancel = function() {
-    if( ! this.tmr ) return;
+	if( ! this.tmr ) return;
 	console.log('timer canceled');
 
-    clearInterval(this.tmr);
-    this.tmr = null;
-    this.timerState = 'canceled';
+	clearInterval(this.tmr);
+	this.tmr = null;
+	this.timerState = 'canceled';
 }
 
+// Runs the callback
 Timer.prototype._handleTmr = function() {
-    this.callbackFunc();
-    this.cancel();
+	this.callbackFunc();
+	this.cancel();
 }
